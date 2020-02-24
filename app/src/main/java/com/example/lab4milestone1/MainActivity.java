b8d9d2188c7b6354533f523129ee2effdaa885f8
klinsmanndennis <klinsmanntje@yahoo.co.id>
2020-02-24 13:16:02 -0600

    milestone 1

--- app/src/main/java/com/example/lab4milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab4milestone1/MainActivity.java
@@ -7,18 +7,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonStartThread;
     private volatile boolean stopThread = false;
     private static final String TAG = "MainActivity";
+    private Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        buttonStartThread = findViewById(R.id.button_start_thread);
+        startBtn = findViewById(R.id.button_start_thread);
     }
 
     public void startThread(View view) {
@@ -31,48 +37,48 @@ public class MainActivity extends AppCompatActivity {
         stopThread = true;
     }
 
-    public class ExampleRunnable implements Runnable {
+
+    class ExampleRunnable implements Runnable {
 
         int seconds;
 
-        ExampleRunnable(int seconds){
+        public ExampleRunnable(int seconds) {
             this.seconds = seconds;
         }
 
         @Override
         public void run() {
-            for(int i = 0; i < seconds; i++) {
-                if(stopThread) {
+            for (int i = 0; i < seconds; i++) {
+                if (stopThread) {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            buttonStartThread.setText("Start");
+                            startBtn.setText("Start");
                         }
                     });
+                    return;
                 }
-                if(i == 5){
+                if (i == 5) {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            buttonStartThread.setText("50%");
+                            startBtn.setText("50%");
                         }
                     });
                 }
                 Log.d(TAG, "startThread: " + i);
                 try {
                     Thread.sleep(1000);
-                }   catch (InterruptedException e) {
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    buttonStartThread.setText("Start");
+                    startBtn.setText("Start");
                 }
             });
         }
     }
-
 }
